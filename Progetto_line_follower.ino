597d072339ad241646ef182d8210e183197d249b
Alessio <150655858+AlessioZarbo@users.noreply.github.com>
2024-01-25 18:30:15 +0100

    Prova1 movimento pippo

---
+++ Progetto_line_follower.ino
@@ -0,0 +1,53 @@
+#include <Servo.h>
+
+Servo destra;
+Servo sinistra;
+
+int const destra_pin = 8; // il destro va al contrario
+int const sinistra_pin = 9;
+
+void setup() {
+  destra.attach(destra_pin);
+  sinistra.attach(sinistra_pin);
+
+}
+
+void staiFermo() {  // Il valore "89" fa stare fermo il servomotore
+
+  destra.write(89); 
+  sinistra.write(89);
+}
+
+void vaiAvanti() {  // Per il destro più tende a "0", più va avanti, per il sinistro più tende a "180", più va avanti
+
+  destra.write(0);  // MAX avanti
+  sinistra.write(180);  // MAX avanti
+}
+
+void vaiADestra90() {
+
+  destra.write(89);
+  sinistra.write(180);
+}
+
+void vaiASinistra90() {
+
+  destra.write(0);
+  sinistra.write(89);
+}
+
+void vaiADestra() { // trovare il giusto rapporto
+
+  destra.write(89);
+  sinistra.write(180);
+}
+
+void vaiASinistra(){  // trovare il giusto rapporto
+
+  destra.write(0);
+  sinistra.write(89);
+}
+
+void loop() {
+  staiFermo();
+}
