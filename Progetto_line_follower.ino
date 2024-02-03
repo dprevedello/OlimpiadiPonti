3926e102304fe84a78c110821a4eb43fb63b83dc
Alessio <150655858+AlessioZarbo@users.noreply.github.com>
2024-02-03 10:41:50 +0100

    Update Fiera

--- Progetto_line_follower.ino
+++ Progetto_line_follower.ino
@@ -1,41 +1,29 @@
 #include <Servo.h>
 
-// Oggetti di tipo Servo
 Servo motore_destra;
 Servo motore_sinistra;
 
-// Collegamenti con in pin
-int const motore_destra_pin = 8; // il destro va al contrario
-int const motore_sinistra_pin = 9; // il sinistro è giusto
-int const sensore_sinistra_pin = A2;
-int const sensore_centro_pin = A1;
-int const sensore_destra_pin = A0;
 
-// Variabili che leggono i sensori
+int const destra_pin = 8; // il destro va al contrario
+int const sinistra_pin = 9; // il sinistro è giusto
+
 int sensore_sinistra = 0;
 int sensore_centro = 0;
 int sensore_destra = 0;
+int soglia = 250; // ex 200
 
-// Variabile per gestire la soglia tra nero e bianco
-int soglia = 200;
-
-// Variabile che ricorda il movimento effettuato precedentemente
 int ritorno = 0;
 
 void setup() {
-  motore_destra.attach(motore_destra_pin);
-  motore_sinistra.attach(motore_sinistra_pin);
-  
-  pinMode(sensore_sinistra_pin, INPUT);
-  pinMode(sensore_centro_pin, INPUT);
-  pinMode(sensore_destra_pin, INPUT);
+  motore_destra.attach(destra_pin);
+  motore_sinistra.attach(sinistra_pin);
 
 }
 
 void staiFermo() {  // Il valore "89" fa stare fermo il servomotore
 
-  motore_destra.write(89); 
-  motore_sinistra.write(89);
+  motore_destra.write(90); 
+  motore_sinistra.write(90);
 }
 
 void vaiAvanti() {  // Per il destro più tende a "0", più va avanti, per il sinistro più tende a "180", più va avanti
@@ -46,62 +34,62 @@ void vaiAvanti() {  // Per il destro più tende a "0", più va avanti, per il si
 
 void vaiADestra90() {
 
-  motore_destra.write(89);
+  motore_destra.write(90);
   motore_sinistra.write(180);
 }
 
 void vaiASinistra90() {
 
   motore_destra.write(0);
-  motore_sinistra.write(89);
+  motore_sinistra.write(90);
 }
 
 void vaiADestra() { // trovare il giusto rapporto
 
-  motore_destra.write(89);
+  motore_destra.write(86);  // ex 88
   motore_sinistra.write(180);
 }
 
 void vaiASinistra(){  // trovare il giusto rapporto
 
   motore_destra.write(0);
-  motore_sinistra.write(89);
+  motore_sinistra.write(94);  // ex 92
 }
 
 void loop() {
 
-  sensore_sinistra = analogRead(sensore_sinistra_pin);
-  sensore_centro = analogRead(sensore_centro_pin);
-  sensore_destra = analogRead(sensore_destra_pin);
+  sensore_sinistra = analogRead(A2);
+  sensore_centro = analogRead(A1);
+  sensore_destra = analogRead(A0);
 
 
   // Se vede tutto nero o tutto bianco
-  if ((sensore_sinistra > 200 && sensore_centro > 200 && sensore_destra > 200) || (sensore_sinistra < 200 && sensore_centro > 200 && sensore_destra < 200)) {
+  if ((sensore_sinistra > soglia && sensore_centro > soglia && sensore_destra > soglia) || (sensore_sinistra < soglia && sensore_centro > soglia && sensore_destra < soglia)) {
     vaiAvanti();
     ritorno = 0;
     }
     // Se c'è una curva di 90 gradi verso sinistra
-    else if(sensore_sinistra > 200 && sensore_centro > 200 && sensore_destra < 200) {
+    else if(sensore_sinistra > soglia && sensore_centro > soglia && sensore_destra < soglia) {
       vaiASinistra90();
       ritorno = 1;
       }
       // Se c'è una curva di 90 gradi verso destra
-      else if(sensore_sinistra < 200 && sensore_centro > 200 && sensore_destra > 200) {
+      else if(sensore_sinistra < soglia && sensore_centro > soglia && sensore_destra > soglia) {
         vaiADestra90();
         ritorno = 2;
         }
         // Se c'è una curva semplice verso sinistra
-        else if(sensore_sinistra > 200 && sensore_centro < 200 && sensore_destra < 200) {
+        else if(sensore_sinistra > soglia && sensore_centro < soglia && sensore_destra < soglia) {
           vaiASinistra();
           ritorno = 3;
           }
           // Se c'è una curva semplice verso destra
-          else if(sensore_sinistra < 200 && sensore_centro < 200 && sensore_destra > 200) {
+          else if(sensore_sinistra < soglia && sensore_centro < soglia && sensore_destra > soglia) {
             vaiADestra();
             ritorno = 4;
             }
             // Se vede tutto bianco
-            else if (sensore_sinistra < 200 && sensore_centro < 200 && sensore_destra < 200) {
+            else if (sensore_sinistra < soglia && sensore_centro < soglia && sensore_destra < soglia) {
               switch (ritorno) {
                 case 0:
                   vaiAvanti();
