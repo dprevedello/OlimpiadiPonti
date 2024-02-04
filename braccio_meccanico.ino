4f1e308bf21d188ee5fc8d41b869b03fa6a3541d
Alessio <150655858+AlessioZarbo@users.noreply.github.com>
2024-02-04 11:29:47 +0100

    Update versione funzionante

--- braccio_meccanico.ino
+++ braccio_meccanico.ino
@@ -1,5 +1,6 @@
-#include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
+
+#include <Wire.h>
 
 Adafruit_PWMServoDriver pwm0 = Adafruit_PWMServoDriver();
 
@@ -36,24 +37,31 @@ int posizione6;
 
 // Variabile che gestisce in quanti "step" il movimento verrá effettuato
 
-int step = 10;
+int step = 60;
+
+// Pin dei Sensori
+
+int const scarico_pin = 2;
+int const carico_pin = 3;
+
+// Variabili che leggono i sensori
+
+int scarico;
+int carico;
 
 
 void setup() {
+  
   pwm0.begin();
   pwm0.setOscillatorFrequency(27000000);
   pwm0.setPWMFreq(SERVO_FREQ);
   delay(10);
+  Serial.begin(9600);
 }
 
-//  Funzione da finire per prendere il cubetto
-
-void prendi() {
+void eretta_senza_presa() {
 
   //  Posizione eretta
-
-  //angolo = 57;  //arrivo base
-  //angolo = 148; // inizio base
 
   angolo1 = 90.0;  //  Posizione di partenza
   posizione1 = map(int(angolo1), 0, 180, SERVOMIN, SERVOMAX);  //  Da mettere sempre
@@ -79,29 +87,85 @@ void prendi() {
   posizione6 = map(int(angolo6), 0, 180, SERVOMIN, SERVOMAX);
   pwm0.setPWM(mot6, 0, posizione6);
 
-  //  Realizzare ciclo per incrementare gradualmente i valori dei servomotori
+  delay(1000);
 
-  // Ciclo per prendere
+}
 
+void eretta_con_presa() {
+
+  // Posizione eretta con presa
+    
+    angolo1 = 90.0;  //  Posizione di partenza
+    posizione1 = map(int(angolo1), 0, 180, SERVOMIN, SERVOMAX);  //  Da mettere sempre
+    pwm0.setPWM(mot1, 0, posizione1);  // Da mettere sempre
+
+    angolo2 = 85.0;  //  Posizione di partenzamo
+    posizione2 = map(int(angolo2), 0, 180, SERVOMIN, SERVOMAX); //  Accetta solo valori interi
+    pwm0.setPWM(mot2, 0, posizione2);
+
+    angolo3 = 93.0;  //  Posizione di partenza
+    posizione3 = map(int(angolo3), 0, 180, SERVOMIN, SERVOMAX);
+    pwm0.setPWM(mot3, 0, posizione3);
+
+    angolo4 = 93.0;  //  Posizione di partenza
+    posizione4 = map(int(angolo4), 0, 180, SERVOMIN, SERVOMAX);
+    pwm0.setPWM(mot4, 0, posizione4);
+
+    angolo5 = 85.0;  //  Posizione di partenza
+    posizione5 = map(int(angolo5), 0, 180, SERVOMIN, SERVOMAX);
+    pwm0.setPWM(mot5, 0, posizione5);
+
+    angolo6 = 55.0; //  Posizione di partenza
+    posizione6 = map(int(angolo6), 0, 180, SERVOMIN, SERVOMAX);
+    pwm0.setPWM(mot6, 0, posizione6);
+
+    delay(1000);
+
+}
+
+// Funzione che prende
+
+void prendi() {
+
+  // Presa
+
+  //  Ciclo per la base
+  
   for(int i = 1; i <= step; i ++) {
 
-    angolo1 = angolo1 + (58.0 / step);
-    posizione1 = map(int(angolo1), 0, 180, SERVOMIN, SERVOMAX); // il problema potrebbe essere che l'arrotondamento da "float" a "int" possa provocare movimenti sbagliati
+    angolo1 = angolo1 + (57.0 / step);
+    posizione1 = map(int(angolo1), 0, 180, SERVOMIN, SERVOMAX);
     pwm0.setPWM(mot1, 0, posizione1);
 
-    angolo2 = angolo2 + (25.0 / step);
-    posizione2 = map(int(angolo2), 0, 180, SERVOMIN, SERVOMAX);
-    pwm0.setPWM(mot2, 0, posizione2);
+    delay(20);
+    
+    }
+
+    // Ciclo per il motore 3 e 4
+
+  for(int i = 1; i <= step; i ++) {
 
     angolo3 = angolo3 + (48.0 / step);
     posizione3 = map(int(angolo3), 0, 180, SERVOMIN, SERVOMAX);
     pwm0.setPWM(mot3, 0, posizione3);
 
     angolo4 = angolo4 + (72.0 / step);
-    posizione4 = map(int(angolo6), 0, 180, SERVOMIN, SERVOMAX);
+    posizione4 = map(int(angolo4), 0, 180, SERVOMIN, SERVOMAX);
     pwm0.setPWM(mot4, 0, posizione4);
 
-    delay(1000);
+    delay(20);
+  }
+
+  // Ciclo per il motore 2
+
+  for(int i = 1; i <= step; i ++) {
+
+    angolo2 = angolo2 + (25.0 / step);
+    posizione2 = map(int(angolo2), 0, 180, SERVOMIN, SERVOMAX);
+    pwm0.setPWM(mot2, 0, posizione2);
+
+    delay(20);
+    
   }
 
   // Presa del motore 6
@@ -111,8 +175,21 @@ void prendi() {
   pwm0.setPWM(mot6, 0, posizione6);
 
   delay(2000);
+  
+  // Ritorno
 
-  // Ciclo per ritornare
+  // Ciclo per il motore 2
+
+  for(int i = 1; i <= step; i ++) {
+    angolo2 = angolo2 - (25.0 / step);
+    posizione2 = map(int(angolo2), 0, 180, SERVOMIN, SERVOMAX); //  Accetta solo valori interi
+    pwm0.setPWM(mot2, 0, posizione2);
+
+    delay(20);
+
+  }
+
+    // Ciclo per il motore 1, 3 e 4
 
   for(int i = 1; i <= step; i ++) {
 
@@ -120,21 +197,136 @@ void prendi() {
     posizione1 = map(int(angolo1), 0, 180, SERVOMIN, SERVOMAX);
     pwm0.setPWM(mot1, 0, posizione1);
 
-    angolo2 = angolo2 - (25.0 / step);
-    posizione2 = map(int(angolo2), 0, 180, SERVOMIN, SERVOMAX); //  Accetta solo valori interi
-    pwm0.setPWM(mot2, 0, posizione2);
-
     angolo3 = angolo3 - (48.0 / step);
     posizione3 = map(int(angolo3), 0, 180, SERVOMIN, SERVOMAX);
     pwm0.setPWM(mot3, 0, posizione3);
 
     angolo4 = angolo4 - (72.0 / step);
-    posizione4 = map(int(angolo6), 0, 180, SERVOMIN, SERVOMAX);
+    posizione4 = map(int(angolo4), 0, 180, SERVOMIN, SERVOMAX);
     pwm0.setPWM(mot4, 0, posizione4);
 
-    delay(500);
+    delay(20);
   }
+
+  delay(1000);
+
 }
 
-void loop() {
+// Funzione per posare
+
+void posa() {
+
+  //  Ciclo per la base
+  
+  for(int i = 1; i <= step; i ++) {
+
+    angolo1 = angolo1 - (36.0 / step);
+    posizione1 = map(int(angolo1), 0, 180, SERVOMIN, SERVOMAX);
+    pwm0.setPWM(mot1, 0, posizione1);
+
+    delay(20);
+    
+    }
+
+    // Ciclo per il motore 3 e 4
+
+  for(int i = 1; i <= step; i ++) {
+
+    angolo3 = angolo3 + (37.0 / step);
+    posizione3 = map(int(angolo3), 0, 180, SERVOMIN, SERVOMAX);
+    pwm0.setPWM(mot3, 0, posizione3);
+
+    angolo4 = angolo4 + (57.0 / step);
+    posizione4 = map(int(angolo4), 0, 180, SERVOMIN, SERVOMAX);
+    pwm0.setPWM(mot4, 0, posizione4);
+
+    delay(20);
+  }
+
+  // Ciclo per il motore 2
+
+  for(int i = 1; i <= step; i ++) {
+
+    angolo2 = angolo2 + (30.0 / step);
+    posizione2 = map(int(angolo2), 0, 180, SERVOMIN, SERVOMAX);
+    pwm0.setPWM(mot2, 0, posizione2);
+
+    delay(20);
+    
+  }
+
+  // Presa del motore 6
+
+  angolo6 = 100;
+  posizione6 = map(int(angolo6), 0, 180, SERVOMIN, SERVOMAX);
+  pwm0.setPWM(mot6, 0, posizione6);
+
+  delay(2000);
+  
+  // Ritorno
+
+  // Ciclo per il motore 2
+
+  for(int i = 1; i <= step; i ++) {
+    angolo2 = angolo2 - (30.0 / step);
+    posizione2 = map(int(angolo2), 0, 180, SERVOMIN, SERVOMAX); //  Accetta solo valori interi
+    pwm0.setPWM(mot2, 0, posizione2);
+
+    delay(20);
+
+  }
+
+    // Ciclo per il motore 1, 3 e 4
+
+  for(int i = 1; i <= step; i ++) {
+
+    angolo1 = angolo1 + (36.0 / step);
+    posizione1 = map(int(angolo1), 0, 180, SERVOMIN, SERVOMAX);
+    pwm0.setPWM(mot1, 0, posizione1);
+
+    angolo3 = angolo3 - (37.0 / step);
+    posizione3 = map(int(angolo3), 0, 180, SERVOMIN, SERVOMAX);
+    pwm0.setPWM(mot3, 0, posizione3);
+
+    angolo4 = angolo4 - (57.0 / step);
+    posizione4 = map(int(angolo4), 0, 180, SERVOMIN, SERVOMAX);
+    pwm0.setPWM(mot4, 0, posizione4);
+
+    delay(20);
+  }
+
+  delay(1000);
+
 }
+
+
+
+void loop() {
+
+  carico = digitalRead(carico_pin);
+  scarico = digitalRead(scarico_pin);
+
+  /* Serial.print("Carico: ");
+  Serial.print(carico);
+  Serial.print("Scarico: ");
+  Serial.print(scarico);
+  delay(2000);  */
+
+  if (carico == 1) {
+    eretta_senza_presa();
+  }
+  else if (carico == 0 && scarico == 1) {
+    eretta_senza_presa();
+    prendi();
+    eretta_con_presa();
+    posa();
+    eretta_senza_presa();
+  }
+  else if (carico == 0 && scarico == 0 ) {
+    eretta_senza_presa();
+  }
+
+
+
+}
+  
